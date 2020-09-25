2cbfdaca0d7aefd85614355b4055460d920acf18
yyeag <yyeagy@gmail.com>
2020-09-25 15:52:09 -0400

    Check2

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,4 +41,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        Uri phone = Uri.parse("tel:0123456789");
+        phoneIntent.setData(phone);
+        startActivity(phoneIntent);
+    }
 }
